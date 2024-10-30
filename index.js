c958a7d27f502dc839b0749eaa99289117f0abc3
samwelkelvin <samwelmunyaka@gmail.com>
2024-10-31 00:48:04 +0300

    Updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Import express
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = 3001;
 
 //Import sqlite3
 const sqlite3 = require('sqlite3').verbose();
